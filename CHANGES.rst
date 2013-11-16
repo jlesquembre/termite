4613c9783d68578f22e799fd28db40367e563daa
Jose Luis Lafuente <jlesquembre@gmail.com>
2013-11-16 23:12:33 +0100

    Preparing release 0.0.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog for termite
 =====================
 
-0.0.1 (unreleased)
+0.0.1 (2013-11-16)
 ------------------
 
 - Initial release
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(here, 'README.rst'), 'rt') as f:
     README = f.read()
 
 setup(name='termite',
-      version='0.0.1.dev0',
+      version='0.0.1',
       author='Jose Luis Lafuente',
       author_email='jlesquembre@gmail.com',
       description='Automates your build process',
